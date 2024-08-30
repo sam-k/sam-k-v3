edc299a3768f79385634906d88593b806540c69d
Sam Kim <21956685+sam-k@users.noreply.github.com>
2024-08-30 04:55:40 -0700

    implement

---
+++ src/components/projects/ProjectIconItem.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+  import GitHubIcon from '../icons/GitHubIcon.vue';
+  import PackageVariantClosedIcon from '../icons/PackageVariantClosedIcon.vue';
+  import type {LinkType} from './utils.ts';
+
+  const props = defineProps<{
+    kind: LinkType;
+    label?: string;
+    link?: string;
+  }>();
+
+  let iconComponent;
+  switch (props.kind) {
+    case 'deploy':
+      iconComponent = PackageVariantClosedIcon;
+      break;
+    case 'github':
+      iconComponent = GitHubIcon;
+      break;
+  }
+</script>
+
+<template>
+  <li>
+    <a
+      :class="[$style.iconContainer, 'noMargin', 'noPadding', 'noUnderline']"
+      :href="link"
+    >
+      <component :is="iconComponent" />
+      <span>{{ label }}</span>
+    </a>
+  </li>
+</template>
+
+<style lang="css" module>
+  .iconContainer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    column-gap: 6px;
+    height: 1.6rem;
+    padding: 0 6px;
+    border: 1px solid var(--color-border);
+    border-radius: 9999px;
+    background-color: var(--color-background-soft);
+    text-decoration: none;
+
+    svg {
+      width: 1.3rem;
+      height: 1.3rem;
+    }
+  }
+</style>
---
+++ src/components/projects/ProjectItem.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+  import ProjectIconItem from './ProjectIconItem.vue';
+  import type {LinkType} from './utils.ts';
+
+  defineProps<{
+    name: string;
+    description: string;
+    icons?: Array<{
+      kind: LinkType;
+      label?: string;
+      link?: string;
+    }>;
+    tags?: Array<{
+      label: string;
+      link?: string;
+    }>;
+  }>();
+</script>
+
+<template>
+  <li :class="$style.container">
+    <!-- TODO: Fetch from data. -->
+    <div :class="$style.testImg" />
+    <div :class="$style.textContainer">
+      <div :class="$style.titleContainer">
+        <span :class="$style.projectName">{{ name }}</span>
+        <ul>
+          <ProjectIconItem
+            v-for="icon in icons"
+            :key="icon.kind"
+            :kind="icon.kind"
+            :label="icon.label"
+            :link="icon.link"
+          />
+        </ul>
+      </div>
+      <span :class="$style.description">{{ description }}</span>
+      <ul :class="$style.tags">
+        <template v-for="tag in tags" :key="tag.label">
+          <li>
+            <a class="noMargin noUnderline" :href="tag.link">{{ tag.label }}</a>
+          </li>
+        </template>
+      </ul>
+    </div>
+  </li>
+</template>
+
+<style module>
+  .container {
+    display: flex;
+    align-items: center;
+    column-gap: 1.5rem;
+  }
+
+  .testImg {
+    padding: 33.75px 60px;
+    background-color: red;
+  }
+
+  .textContainer {
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.5rem;
+  }
+
+  .titleContainer {
+    display: flex;
+    align-items: center;
+    column-gap: 0.5rem;
+
+    .projectName {
+      font-weight: 600;
+      font-size: 1.3rem;
+    }
+
+    ul {
+      display: flex;
+      column-gap: 0.5rem;
+    }
+  }
+
+  .description {
+    font-size: 1.2rem;
+  }
+
+  .tags {
+    display: flex;
+    column-gap: 0.5rem;
+
+    a {
+      background-color: var(--color-background-mute);
+      text-decoration: none;
+    }
+  }
+</style>
