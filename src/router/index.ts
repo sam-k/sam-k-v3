b251f229685308082e76b99ce61d6363cd0a4469
Sam Kim <21956685+sam-k@users.noreply.github.com>
2024-09-04 02:41:25 -0700

    revert router test

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,15 +2,6 @@ import {createRouter, createWebHistory} from 'vue-router';
 
 export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  scrollBehavior(to, from, savedPosition) {
-    if (to.hash) {
-      return {
-        el: to.hash,
-        behavior: 'smooth',
-      };
-    }
-    return {top: 0};
-  },
   routes: [
     {
       path: '/',
