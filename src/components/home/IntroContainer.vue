e622df31d97af115cf01c294e9af8c41c8e6fd77
Sam Kim <21956685+sam-k@users.noreply.github.com>
2025-05-02 01:55:12 -0700

    Add intro container, fix build script (#26)

---
+++ src/components/home/IntroContainer.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import {getIntro} from '../../api/index.ts';
+  import ContactsBox from './ContactsBox.vue';
+
+  const introData = await getIntro();
+</script>
+
+<template>
+  <div :class="$style.introContainer">
+    <div :class="$style.introTextContainer">
+      <h1>{{ introData.title }}</h1>
+      <p v-for="(line, index) in introData.lines" :key="index">{{ line }}</p>
+    </div>
+    <ContactsBox />
+  </div>
+</template>
+
+<style module>
+  .introContainer {
+    display: flex;
+    justify-content: space-between;
+    gap: 3rem;
+    padding: 2.5rem 0;
+
+    @media (width < 1024px) {
+      flex-direction: column;
+      gap: 3rem;
+      padding: 0;
+    }
+  }
+
+  .introTextContainer {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+</style>
